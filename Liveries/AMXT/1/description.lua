dcad1f5963efd3dcf5bd01af6ee37f85a8578df3
Denis <denissoliveira@gmail.com>
2026-06-19 18:45:27 -0300

    feat: add liveries config

---
+++ Liveries/AMXT/1/description.lua
@@ -0,0 +1,74 @@
+livery = {
+	{ "10 - Default",                         DIFFUSE,       "canopyglass",                  true },
+	{ "10 - Default",                         SPECULAR,      "sm_canopyglass",               true },
+	{ "10 - Default",                         11,            "sm_canopyglass",               true },
+	{ "MascM",                                DIFFUSE,       "capmasks",                     true },
+	{ "MascM",                                NORMAL_MAP,    "capmasks_nm",                  true },
+	{ "AT27_PARTS",                           DIFFUSE,       "at-27_parts",                  true },
+	{ "AT27_PARTS",                           NORMAL_MAP,    "at-27_parts_nm",               true },
+	{ "19 - Default",                         DIFFUSE,       "amx_03",                       true },
+	{ "19 - Default",                         NORMAL_MAP,    "bm_amx_03",                    true },
+	{ "19 - Default",                         SPECULAR,      "sm_amx_03",                    true },
+	{ "19 - Default",                         11,            "sm_amx_03",                    true },
+	{ "CAP01",                                DIFFUSE,       "cappilot01",                   true },
+	{ "CAP01",                                NORMAL_MAP,    "cappilot01_nm",                true },
+	{ "AMX_FUSE01",                           DIFFUSE,       "amx_fuse_01",                  true },
+	{ "AMX_FUSE01",                           NORMAL_MAP,    "amx_fuse_01_nm",               true },
+	{ "Helmet_glass",                         DIFFUSE,       "cap_viseira",                  true },
+	{ "8314703f_9560_4b81_aaf4_dfeb8f84712d", DIFFUSE,       "amx_03",                       true },
+	{ "8314703f_9560_4b81_aaf4_dfeb8f84712d", 11,            "amx_a_1a_brazil_interior_n_s", true },
+	{ "AT27SEAT",                             DIFFUSE,       "at-27_seat",                   true },
+	{ "AT27SEAT",                             NORMAL_MAP,    "at-27_seat_nm",                true },
+	{ "07 - Default",                         DIFFUSE,       "amx_parts",                    true },
+	{ "07 - Default",                         NORMAL_MAP,    "bm_amx_parts",                 true },
+	{ "07 - Default",                         SPECULAR,      "sm_amx_parts",                 true },
+	{ "07 - Default",                         11,            "sm_amx_parts",                 true },
+	{ "AMX_parts",                            DIFFUSE,       "amx_parts",                    true },
+	{ "AMX_parts",                            NORMAL_MAP,    "amx_parts_nm",                 true },
+	{ "glassAMX",                             DIFFUSE,       "amx_glass",                    true },
+	{ "glassAMX",                             NORMAL_MAP,    "amx_glass_nm",                 true },
+	{ "08 - Default",                         DIFFUSE,       "amx_01",                       true },
+	{ "08 - Default",                         NORMAL_MAP,    "bm_amx_01",                    true },
+	{ "08 - Default",                         SPECULAR,      "sm_amx_01",                    true },
+	{ "08 - Default",                         11,            "sm_amx_01",                    true },
+	{ "267f1548_42e6_4792_939f_5b536c096277", DIFFUSE,       "amx_02",                       true },
+	{ "AT27_PILOT",                           DIFFUSE,       "pilot_al",                     true },
+	{ "AT27_PILOT",                           NORMAL_MAP,    "pilot_al_nm",                  true },
+	{ "8314703f_9560_4b81_aaf4_dfeb8f84712d", DIFFUSE,       "amx_a_1a_brazil_interior_c",   true },
+	{ "8314703f_9560_4b81_aaf4_dfeb8f84712d", 11,            "amx_a_1a_brazil_interior_n_s", true },
+	{ "AT27_PILOT",                           DIFFUSE,       "pilot_al",                     true },
+	{ "AT27_PILOT",                           NORMAL_MAP,    "pilot_al_nm",                  true },
+	{ "02 - Default",                         DIFFUSE,       "amx_02",                       true },
+	{ "02 - Default",                         NORMAL_MAP,    "bm_amx_02",                    true },
+	{ "02 - Default",                         SPECULAR,      "sm_amx_02",                    true },
+	{ "02 - Default",                         11,            "sm_amx_02",                    true },
+	{ "267f1548_42e6_4792_939f_5b536c096277", DIFFUSE,       "amx_a_1a_brazil_c",            true },
+	{ "267f1548_42e6_4792_939f_5b536c096277", 11,            "amx_a_1a_brazil_n_s",          true },
+	{ "CAP02",                                DIFFUSE,       "cappilot02",                   true },
+	{ "CAP02",                                NORMAL_MAP,    "cappilot02_nm",                true },
+	{ "22d8c774_0651_4725_893b_f8d45310a3db", DIFFUSE,       "amx_01",                       true },
+	{ "WingAMX",                              DIFFUSE,       "amx_wings",                    true },
+	{ "WingAMX",                              NORMAL_MAP,    "amx_wings_nm",                 true },
+	{ "AMX_FUSE02",                           DIFFUSE,       "amx_fuse_02",                  true },
+	{ "AMX_FUSE02",                           NORMAL_MAP,    "amx_fuse_02_nm",               true },
+	{ "13 - Default",                         DIFFUSE,       "amx_04",                       true },
+	{ "13 - Default",                         NORMAL_MAP,    "bm_amx_04",                    true },
+	{ "13 - Default",                         SPECULAR,      "sm_amx_04",                    true },
+	{ "13 - Default",                         11,            "sm_amx_04",                    true },
+}
+
+name = "AMX-T"
+countries = {}
+custom_args =
+{
+	[810] = 0, -- Pilot helmet
+	[811] = 0, -- Helmet visor 0-close, 1-open
+	[812] = 0, -- 0: colete, 1: no colete
+
+	[814] = 0.5, -- Pilot helmet
+	[815] = 1, -- Helmet visor 0-close, 1-open
+	[816] = 1, -- 0: colete, 1: no colete
+	
+	[999] = 1, -- AMX-T: 1, AMX-A: 0
+
+}
